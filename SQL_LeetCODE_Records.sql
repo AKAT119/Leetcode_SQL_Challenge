147a5f94ff6b6be2469c4376df0e251d9afbc1bd
Azin Katiraee <azinnash@yahoo.com>
2025-03-26 22:34:51 -0400

    1978. Employees Whose Manager Left the Company

--- SQL_LeetCODE_Records.sql
+++ SQL_LeetCODE_Records.sql
@@ -257,3 +257,8 @@ SELECT CONCAT('There are a total of ', COUNT(occupation), ' ', lower(occupation)
 FROM Occupations
 GROUP BY occupation
 ORDER BY COUNT(occupation), occupation;
+#-------------------------------------------
+SELECT employee_id
+FROM employees
+WHERE salary <30000 AND manager_id  NOT IN (SELECT employee_id from employees)
+ORDER BY employee_id;
