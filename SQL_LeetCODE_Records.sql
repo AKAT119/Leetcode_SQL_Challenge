cdee6a65ba8ebeaa9815899d3ad7824e5e3f9bea
Azin Katiraee <azinnash@yahoo.com>
2025-04-14 23:25:55 -0400

    1484. Group Sold Products By The Date

--- SQL_LeetCODE_Records.sql
+++ SQL_LeetCODE_Records.sql
@@ -271,3 +271,12 @@ ORDER BY user_id;
 DELETE p2
 FROM Person p1
 JOIN Person p2 ON p1.email = p2.email AND p1.id < p2.id;
+#----------------------
+# Write your MySQL query statement below
+SELECT 
+  sell_date, 
+  COUNT(DISTINCT product) AS num_sold, 
+  GROUP_CONCAT(DISTINCT product ORDER BY product ASC) AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date ASC;
