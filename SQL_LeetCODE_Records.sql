284642fbb629303ea8a4e445252f45b83a40fbd0
Azin Katiraee <azinnash@yahoo.com>
2025-04-16 14:02:06 -0400

    1327. List the Products Ordered in a Period

--- SQL_LeetCODE_Records.sql
+++ SQL_LeetCODE_Records.sql
@@ -280,3 +280,10 @@ SELECT
 FROM Activities
 GROUP BY sell_date
 ORDER BY sell_date ASC;
+#---------------------------------
+SELECT product_name, SUM(unit) AS unit
+FROM products p JOIN orders o
+ON p.product_id = o.product_id
+WHERE YEAR(o.order_date)='2020' AND MONTH(o.order_date)='02'
+GROUP BY p.product_id
+HAVING SUM(unit) >= 100;
